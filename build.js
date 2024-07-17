25d0c31d0d7b50b3c46b0bcaa28dcb866f2e26ed
julianollivieira <julian.ollivieira@gmail.com>
2024-07-17 16:47:01 +0200

    add css support + tailwind configuration and configure esbuild to auto import jsx functions

--- build.js
+++ build.js
@@ -1,15 +1,61 @@
 import * as esbuild from "esbuild";
+import postcss from "postcss";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 import sneeuw from "@sneeuwphp/esbuild-plugin-sneeuw";
+import fs from "fs";
+import chokidar from "chokidar";
 
-await esbuild.build({
-	entryPoints: ["src/ssr.ts"],
+const tailwindConfig = {
+	content: ["./src/**/*.{html,ski}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
+
+const tailwindPlugin = {
+	name: "sneeuw-tailwind",
+	setup(build) {
+		build.onLoad({ filter: /\.css$/ }, async ({ path }) => {
+			const postcssInstance = postcss().use(
+				tailwindcss(tailwindConfig),
+				autoprefixer(),
+			);
+
+			const file = fs.readFileSync(path);
+			const processed = await postcssInstance.process(file, {
+				from: undefined,
+			});
+
+			return { contents: processed.css, loader: "css" };
+		});
+	},
+};
+
+const ctx = await esbuild.context({
+	entryPoints: ["./src/ssr.ts"],
+	outfile: "ssr.cjs",
 	platform: "node",
 	bundle: true,
 	plugins: [sneeuw],
-	outfile: "out.js",
-	jsxFactory: "h",
-	jsxFragment: "Fragment",
-	loader: {
-		".html": "text",
-	},
+	jsx: "automatic",
+	jsxImportSource: "@sneeuwphp/ski",
+	loader: { ".html": "text" },
 });
+
+const stylesCtx = await esbuild.context({
+	entryPoints: ["./src/styles/app.css"],
+	outfile: "./public/styles.css",
+	plugins: [tailwindPlugin],
+});
+
+chokidar.watch("./src/**/*.{html,ski}").on("change", async (path) => {
+	console.log(`${path} has changed, triggering rebuild...`);
+	await stylesCtx.rebuild();
+});
+
+await ctx.watch();
+await stylesCtx.watch();
+
+console.log("watching for file changes...");
--- public/index.php
+++ public/index.php
@@ -3,13 +3,22 @@
 use Sneeuw\Application;
 use Sneeuw\Http\Request;
 
+$requestUri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+if ($requestUri !== '/' && file_exists(__DIR__.$requestUri)) {
+    header('Content-Type: text/css');
+    readfile(__DIR__.$requestUri);
+
+    return true;
+}
+
 // Register the Composer autoloader
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Sneeuw and handle the request
 $app = (new Application())
     ->withFileBasedRoutes(
-        __DIR__.'/../src/pages'
+        pages: realpath(__DIR__.'/../src/pages'),
+        components: realpath(__DIR__.'/../src/components')
     );
 
 $app->handle(Request::capture());
